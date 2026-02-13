c5262a66c5eb76cad9804b9520c2c57156c75985
yueyang <pyylppppx382@gmail.com>
2026-02-13 11:40:23 -0500

    connecting and testing supabase

--- student-planner/src/App.tsx
+++ student-planner/src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { supabase } from "./lib/supabase"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  //test supabase connection(status: good connection)
+  async function testConnection() {
+    const{data, error} = await supabase.from("student_signUp").select("*")
+    console.log("data",data)
+    console.log("error",error)
+  }
 
   return (
     <>
-     <p>Hello</p>
+    <button onClick={()=>testConnection()} >click</button>
+    
     </>
   )
 }
